9d5f5d22097e9688cab4180596fbf4580ffec18e
Lucile Lestrade <lucilecharly.lestrade@gmail.com>
2020-03-21 19:35:15 +0100

    add model mutation

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_18_212839) do
+ActiveRecord::Schema.define(version: 2020_03_21_183444) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "mutations", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
