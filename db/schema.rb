d6b282f787453a85a92a2a1a9470e597a46a2b9e
Lucile Lestrade <lucilecharly.lestrade@gmail.com>
2020-03-21 19:40:05 +0100

    add model relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_21_183444) do
+ActiveRecord::Schema.define(version: 2020_03_21_183809) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "mutations", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.bigint "adult_1_id", null: false
+    t.bigint "adult_2_id"
+    t.bigint "adult_3_id"
+    t.bigint "kid_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
