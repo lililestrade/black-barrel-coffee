d21c396e3eea6529739007c62c1b3d66346f95df
Lucile Lestrade <lucilecharly.lestrade@gmail.com>
2020-03-22 12:45:36 +0100

    mutation seed okay

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Destroying database..."
+
+Mutation.destroy_all
+
+puts "Creatings mutations ..."
+
+natural_mutation = Mutation.create(name: "Naturally occurring genetic mutation.")
+hybrid_one = Mutation.create(name: "Hybrid mutation naturally occurring.")
+hybrid_two = Mutation.create(name: "Hybrid mutation selective breeding.")
+
+puts 'Finish !'
