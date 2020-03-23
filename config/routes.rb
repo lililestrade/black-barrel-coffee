37f85a179bb9e703c404ee07abed1242c53eed11
Lucile Lestrade <lucilecharly.lestrade@gmail.com>
2020-03-23 19:36:19 +0100

    routes okay

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,17 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
+  resources :drying_processes
+  resources :countries
   resources :varieties
-  resources :relationships, only: [:edit, :update]
+  resources :parents, only: [:edit, :update]
+  resources :productive_country, only: [:edit, :update]
+  resources :country_process_links, only: [:edit, :update]
+  resources :harvest_periods, only: [:edit, :update]
+
+  # resources :productive_country, only: [:new, :create]
+  # resources :country_process_links, only: [:new, :create]
+  # resources :harvest_periods, only: [:new, :create]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
