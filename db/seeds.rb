013098779e65ca422c331fffb258f011e1cac373
Lucile Lestrade <lucilecharly.lestrade@gmail.com>
2020-03-23 19:26:49 +0100

    seed okay

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,11 +9,27 @@
 puts "Destroying database..."
 
 Mutation.destroy_all
+Month.destroy_all
 
 puts "Creatings mutations ..."
 
 natural_mutation = Mutation.create(name: "Naturally occurring genetic mutation.")
 hybrid_one = Mutation.create(name: "Hybrid mutation naturally occurring.")
 hybrid_two = Mutation.create(name: "Hybrid mutation selective breeding.")
+
+puts "Creatings months ..."
+
+Month.create(name: "Janvier")
+Month.create(name: "Février")
+Month.create(name: "Mars")
+Month.create(name: "Avril")
+Month.create(name: "Mai")
+Month.create(name: "Juin")
+Month.create(name: "Juillet")
+Month.create(name: "Aout")
+Month.create(name: "Septembre")
+Month.create(name: "Octobre")
+Month.create(name: "Novembre")
+Month.create(name: "Décembre")
 
 puts 'Finish !'
