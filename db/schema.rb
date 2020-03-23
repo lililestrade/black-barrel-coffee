b0584869488b13b66666a4528da67b2651c8e7bd
Lucile Lestrade <lucilecharly.lestrade@gmail.com>
2020-03-23 17:10:14 +0100

    add model Month

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_21_183444) do
+ActiveRecord::Schema.define(version: 2020_03_23_160852) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "months", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "mutations", force: :cascade do |t|
     t.string "name"
