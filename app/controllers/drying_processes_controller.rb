33da480d31601c7e32adc6d94db08e21d95c0937
Lucile Lestrade <lucilecharly.lestrade@gmail.com>
2020-03-23 22:22:48 +0100

    DryingProcess #new #create

--- app/controllers/drying_processes_controller.rb
+++ app/controllers/drying_processes_controller.rb
@@ -6,4 +6,23 @@ class DryingProcessesController < ApplicationController
   def show
     @drying_process = DryingProcess.find(params[:id])
   end
+
+  def new
+    @drying_process = DryingProcess.new
+  end
+
+  def create
+    @drying_process = DryingProcess.new(drying_process_params)
+    if @drying_process.save!
+      redirect_to drying_processes_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def drying_process_params
+    params.require(:drying_process).permit(:name, :process_time, :description)
+  end
 end
