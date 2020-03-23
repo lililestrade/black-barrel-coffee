bfb33d2bae26823554ac676d2dc42d7f59a22bcd
Lucile Lestrade <lucilecharly.lestrade@gmail.com>
2020-03-23 17:04:38 +0100

    restart db

--- app/models/relationship.rb
+++
@@ -1,6 +0,0 @@
-class Relationship < ApplicationRecord
-  has_many :varieties
-
-  validates :adult_1_id, presence: true
-  validates :kid_id, presence: true
-end
--- app/models/variety.rb
+++
@@ -1,7 +0,0 @@
-class Variety < ApplicationRecord
-  belongs_to :mutation
-  has_many :relationship
-
-  validates :name, presence: true, uniqueness: true
-
-end
