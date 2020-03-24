2c315a3e1d39b53c32cadd3b7d37a8d36460a7c7
Lucile Lestrade <lucilecharly.lestrade@gmail.com>
2020-03-24 19:21:39 +0100

    controller variety index

---
+++ app/policies/variety_policy.rb
@@ -0,0 +1,17 @@
+class VarietyPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      if user.admin?
+        scope.all
+      end
+    end
+  end
+
+  def show?
+    return true
+  end
+
+  def create?
+    return true if user.admin?
+  end
+end
