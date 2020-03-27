c95bdd82839244f400a5c268205048a9439b9df3
Lucile Lestrade <lucilecharly.lestrade@gmail.com>
2020-03-27 15:42:10 +0100

    mapbox search

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,11 @@
+// CSS
 import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
+// internal import
 import { initMapbox } from '../plugins/init_mapbox';
-
 initMapbox();
+
+// import { initAutocomplete } from '../plugins/init_autocomplete';
+// initAutocomplete();
--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,4 +1,5 @@
 import mapboxgl from 'mapbox-gl';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
@@ -11,21 +12,29 @@ const initMapbox = () => {
 
   if (mapElement) { // only build a map if there's a div#map to inject into
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+
     const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10'
+      // style: 'mapbox://styles/mapbox/streets-v10'
+      style: 'mapbox://styles/mapbox/streets-v11'
     });
+
     const markers = JSON.parse(mapElement.dataset.markers);
 
     markers.forEach((marker) => {
       const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
+
       new mapboxgl.Marker()
         .setLngLat([ marker.lng, marker.lat ])
         .setPopup(popup) // add this
         .addTo(map);
-      });
-      fitMapToMarkers(map, markers);
-    }
+    });
+
+    fitMapToMarkers(map, markers);
+
+    map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
+                                        mapboxgl: mapboxgl }));
+  }
 };
 
 export { initMapbox };
