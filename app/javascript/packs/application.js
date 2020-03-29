fea1dfa98065072660d91a239a456f1baf6273d2
Lucile Lestrade <lucilecharly.lestrade@gmail.com>
2020-03-29 13:42:55 +0200

    pointer with circle

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,3 +9,22 @@ initMapbox();
 
 // import { initAutocomplete } from '../plugins/init_autocomplete';
 // initAutocomplete();
+
+// Pointer
+jQuery(document).ready(function() {
+
+  var mouseX = 0, mouseY = 0;
+  var xp = 0, yp = 0;
+
+  $(document).mousemove(function(e){
+    mouseX = e.pageX - 20;
+    mouseY = e.pageY - 20;
+  });
+
+  setInterval(function(){
+    xp += ((mouseX - xp)/6);
+    yp += ((mouseY - yp)/6);
+    $("#circle").css({left: xp +'px', top: yp +'px'});
+  }, 20);
+
+});
