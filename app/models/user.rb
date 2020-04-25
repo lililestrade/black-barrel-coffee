debefabd086878bcf965fab0d2b7fff7ba9bffa3
Lucile Lestrade <lucilecharly.lestrade@gmail.com>
2020-04-25 20:10:37 +0200

    removed :registerable from user for no sign_up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable, :registerable,
+  devise :database_authenticatable, # removed :registerable for no sign_up
          :recoverable, :rememberable, :validatable
 
   validates :email, uniqueness: true, presence: true
